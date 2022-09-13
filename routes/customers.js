369724d56c08a8f60b9d8725f031e294907bf374
GriffithDavis28 <griffithdavis1999@gmail.com>
2022-09-13 15:25:40 +0530

    updaed customers and orders

--- routes/customers.js
+++ routes/customers.js
@@ -64,56 +64,56 @@ router.get('/', (req,res) => {
       //     return;
       // }
       const customer = {
-          id: req.body.customerNumber,
-          customerName: req.body.customerName,
-          contactLastName: req.body.contactLastName,
-          contactFirstName: req.body.contactFirstName,
-          phone: req.body.phone,
-          addressLine1: req.body.addressLine1,
-          addressLine2: req.body.addressLine2,
-          city: req.body.city,
-          state: req.body.state,
-          postalCode: req.body.postalCode,
-          country: req.body.country,
-          salesRepEmployeeNumber: req.body.salesRepEmployeeNumber,
-          creditLimit: req.body.creditLimit 
-      };
-      console.log(customer);
-      const mysql = require('mysql')
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Griffith@99',
-      database: 'classicmodels'
-    })
-    
-    connection.connect()
-    customerNumber= parseInt(req.params.id);
-    customerquery='INSERT INTO classicmodels.customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit)'
-    customerquery=customerquery+ ' VALUES ('+customer.id+" , '"+ customer.customerName+"'"
-    customerquery=customerquery+" , '"+customer.contactFirstName+"'"
-    customerquery=customerquery+" , '"+customer.contactLastName+"'"
-    customerquery=customerquery+" , '"+customer.phone+"'"
-    customerquery=customerquery+" , '"+customer.addressLine1+"'"
-    customerquery=customerquery+" , '"+customer.addressLine2+"'"
-    customerquery=customerquery+" , '"+customer.city+"'"
-    customerquery=customerquery+" , '"+customer.state+"'"
-    customerquery=customerquery+" , '"+customer.postalCode+"'"
-    customerquery=customerquery+" , '"+customer.country+"'"
-    customerquery=customerquery+" , "+customer.salesRepEmployeeNumber+""
-    customerquery=customerquery+" , "+customer.creditLimit+")";
-    console.log(customerquery); 
-    connection.query(customerquery, (err, result) => {
-      if (err){
-        res.status(202).send("Errors found")
-      return;
-     }
-        else
-        {
-        res.status(200).send("Record added successfully")
-      return;
-      }
-  });
+        id: req.body.customerNumber,
+        customerName: req.body.customerName,
+        contactLastName: req.body.contactLastName,
+        contactFirstName: req.body.contactFirstName,
+        phone: req.body.phone,
+        addressLine1: req.body.addressLine1,
+        addressLine2: req.body.addressLine2,
+        city: req.body.city,
+        state: req.body.state,
+        postalCode: req.body.postalCode,
+        country: req.body.country,
+        salesRepEmployeeNumber: req.body.salesRepEmployeeNumber,
+        creditLimit: req.body.creditLimit 
+    };
+    console.log(customer);
+    const mysql = require('mysql')
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Griffith@99',
+    database: 'classicmodels'
+  })
+  
+  connection.connect()
+  customerNumber= parseInt(req.params.id);
+  customerquery='INSERT INTO classicmodels.customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit)'
+  customerquery=customerquery+ ' VALUES ('+customer.id+" , '"+ customer.customerName+"'"
+  customerquery=customerquery+" , '"+customer.contactFirstName+"'"
+  customerquery=customerquery+" , '"+customer.contactLastName+"'"
+  customerquery=customerquery+" , '"+customer.phone+"'"
+  customerquery=customerquery+" , '"+customer.addressLine1+"'"
+  customerquery=customerquery+" , '"+customer.addressLine2+"'"
+  customerquery=customerquery+" , '"+customer.city+"'"
+  customerquery=customerquery+" , '"+customer.state+"'"
+  customerquery=customerquery+" , '"+customer.postalCode+"'"
+  customerquery=customerquery+" , '"+customer.country+"'"
+  customerquery=customerquery+" , "+customer.salesRepEmployeeNumber+""
+  customerquery=customerquery+" , "+customer.creditLimit+")";
+  console.log(customerquery); 
+  connection.query(customerquery, (err, result) => {
+    if (err){
+      res.status(202).send("Customer details  already exist")
+    return;
+   }
+      else
+      {
+      res.status(200).send("Record added successfully")
+    return;
+    }
+});
 })
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -12,9 +12,9 @@ router.get('/', (req,res) => {
     })
     
     connection.connect()
-    customerquery='SELECT *  FROM orders';
-    console.log(customerquery); 
-    connection.query(customerquery, (err, rows, fields) => {
+    orderquery='SELECT *  FROM orders';
+    console.log(orderquery); 
+    connection.query(orderquery, (err, rows, fields) => {
       if (err) throw err
     
       console.log('The solution is: ', rows[0])
@@ -33,10 +33,10 @@ router.get('/', (req,res) => {
     
     connection.connect()
     customerNumber= parseInt(req.params.id);
-    customerquery='SELECT c.customerNumber, o.orderNumber , o.orderDate , customerName, contactFirstName, contactLastName FROM customers c INNER JOIN orders o on c.customerNumber = o.customerNumber';
-    customerquery=customerquery+' where c.customerNumber = '+customerNumber;
-    console.log(customerquery); 
-    connection.query(customerquery, (err, rows, fields) => {
+    orderquery='SELECT c.customerNumber, o.orderNumber , o.orderDate , customerName, contactFirstName, contactLastName FROM customers c INNER JOIN orders o on c.customerNumber = o.customerNumber';
+    orderquery=orderquery+' where c.customerNumber = '+customerNumber;
+    console.log(orderquery); 
+    connection.query(orderquery, (err, rows, fields) => {
       if (err) throw err
     
       console.log('The solution is: ', rows[0])
@@ -51,23 +51,62 @@ router.get('/', (req,res) => {
   });
   
   router.post('/', (req, res) => {
-      const schema = Joi.object({
-          id: Joi.string().min(1).required(),
-          name: Joi.string().min(3).required()
-      });
-      const validation = schema.validate(req.body);
-      res.send(validation);
+      // const schema = Joi.object({
+      //     id: Joi.string().min(1).required(),
+      //     name: Joi.string().min(3).required()
+      // });
+      // const validation = schema.validate(req.body);
+      // res.send(validation);
   
-      if(!req.body.name || req.body.name.length < 3) {
-          res.status(400).send("Name should be at least 3 characters");
-          return;
+      // if(!req.body.name || req.body.name.length < 3) {
+      //     res.status(400).send("Name should be at least 3 characters");
+      //     return;
+      // }
+      // const customer = {
+      //     id: Customers.length + 1,
+      //     name: req.body.name
+      // };
+      // Customers.push|(customer);
+      // res.send(customer);
+      const order = {
+        orderNumber: req.body.orderNumber,
+        orderDate: req.body.orderDate,
+        requiredDate: req.body.requiredDate, 
+        shippedDate: req.body.shippedDate, 
+        status: req.body.status, 
+        comments: req.body.comments, 
+        customerNumber: req.body.customerNumber
+       };
+      console.log(order);
+      const mysql = require('mysql')
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'Griffith@99',
+      database: 'classicmodels'
+    })
+    
+    connection.connect()
+    customerNumber= parseInt(req.params.id);
+    orderquery='INSERT INTO classicmodels.orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)'
+    orderquery=orderquery+ ' VALUES ('+order.orderNumber+" , '"+ order.orderDate+"'"
+    orderquery=orderquery+" , '"+order.requiredDate+"'"
+    orderquery=orderquery+" , '"+order.shippedDate+"'"
+    orderquery=orderquery+" , '"+order.status+"'"
+    orderquery=orderquery+" , '"+order.comments+"'"
+    orderquery=orderquery+" , "+order.customerNumber+")";
+    console.log(orderquery); 
+    connection.query(orderquery, (err, result) => {
+      if (err){
+        res.status(202).send("Error: Order has already been placed")
+      return;
+     }
+        else
+        {
+        res.status(200).send("Order successfully placed")
+      return;
       }
-      const customer = {
-          id: Customers.length + 1,
-          name: req.body.name
-      };
-      Customers.push|(customer);
-      res.send(customer);
   });
+});
 
 module.exports = router;
